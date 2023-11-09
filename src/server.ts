8b1174b273e9bd032aaed5338598b857c89d5a01
Kokmeng.Oou <kokmeng.ou69@gmail.com>
2023-11-09 12:45:42 +0000

    error deploying

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
 import express from 'express'
-import cors from 'cors'
+// import cors from 'cors'
 // import cookieParser from 'cookie-parser'
-import helmet from 'helmet'
+// import helmet from 'helmet'
 
 // connect db
-import connectDB from './config/db_connect'
+// import connectDB from './config/db_connect'
 import './config/dotenv'
 // import authRoutes from './routes/authRoutes'
 
 const app: express.Application = express()
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
 // app.use(cookieParser(process.env.COOKIE_SECRET))
-app.use(helmet())
+// app.use(helmet())
 
 const port: number = 5000
 
@@ -31,12 +31,12 @@ app.get(
 
 async function runServer(): Promise<void> {
   try {
-    await connectDB((process.env as any).CONNECT_DB_URL)
+    // await connectDB((process.env as any).CONNECT_DB_URL)
     app.listen(port, () => {
       console.log(`Server is running on port http://localhost:${port}`)
     })
   } catch (error) {
-    throw error
+    console.log(error)
   }
 }
 runServer().catch((err) => console.log('Error connecting to DB'))
