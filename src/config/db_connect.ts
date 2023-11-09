f116acc2900cbbf50b8af4d8776287042a4ad92a
Kokmeng.Oou <kokmeng.ou69@gmail.com>
2023-11-09 17:37:17 +0000

    update

--- src/config/db_connect.ts
+++ src/config/db_connect.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'
 
 export default function connectDB(): Promise<typeof mongoose> {
+  mongoose.set('strictQuery', false)
   return mongoose.connect((process.env as any).CONNECT_DB_URL)
 }
